a5194064bc8b7189a26eb5afac12586162c7ca14
galenczk <ciszekg@oregonstate.edu>
2022-10-30 15:51:37 -0700

    Set up POST route handler charts.

---
+++ api/index.mjs
@@ -0,0 +1,39 @@
+// Import dependencies
+import express from "express";
+import asyncHandler from "express-async-handler";
+import cors from "cors";
+import QuickChart from "quickchart-js";
+
+// Init express app
+const app = express();
+const PORT = 3001;
+//app.use(express.json);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+// Init chart object
+const newChart = new QuickChart();
+
+// POST request with chart settings returns URL of rendered chart
+app.post(
+  "/api",
+  asyncHandler(async (req, res) => {
+    newChart.setConfig({
+      type: "pie",
+      data: req.body,
+    });
+    console.log(req.body);
+    res.status(200).json({ url: newChart.getUrl() });
+  })
+);
+
+app.use((error, req, res, next) => {
+  console.log(`Unhandled error ${error}, URL=${req.originalURL},
+  method=${req.method}`);
+  res.send("500 -- Server Error");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
